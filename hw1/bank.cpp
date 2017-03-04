ad754c01ea9f99fe803a75df84f9fecb3dd95c89
dimiturtrz <dimitar.trz@gmail.com>
2017-03-04 21:36:23 +0200

    many problems with dynamically allocated objects later and still some more for problem 3

---
+++ hw1/bank.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+void strcpy(char* dest, const char* source);
+int strlen(const char* str);
+
+struct Person {
+protected:
+	char* firstName;
+	char* lastName;
+public:
+	char* getFirstName() const {
+		return firstName;
+	}
+
+	char* getLastName() const {
+		return firstName;
+	}
+
+	void setFirstName(const char* newFirstName) {
+		firstName = new char[strlen(newFirstName)];
+		strcpy(firstName, newFirstName);
+	}
+
+	void setLastName(const char* newLastName) {
+		lastName = new char[strlen(newLastName)];
+		strcpy(lastName, newLastName);
+	}
+
+	Person() {
+		firstName = NULL;
+		lastName = NULL;
+	}
+	Person(const char* newFirstName, const char* newLastName) {
+		setFirstName(newFirstName);
+		setLastName(newLastName);
+	}
+
+	Person(const Person& other) {
+		setFirstName(other.firstName);
+		setLastName(other.lastName);
+	}
+
+	Person& operator=(const Person& other) {
+		setFirstName(other.firstName);
+		setLastName(other.lastName);
+	}
+
+	~Person() {
+		delete [] firstName;
+		firstName = NULL;
+		delete [] lastName;
+		lastName = NULL;
+	}
+};
+
+struct Client: Person {
+private:
+	float balance;
+
+public:
+	float getBalance() const {
+		return balance;
+	}
+
+	void setBalance(float newBalance) {
+		balance = newBalance;
+	}
+
+	Client() {}
+	Client(const char* newFirstName, const char* newLastName, float newBalance) : Person(newFirstName, newLastName) {
+		balance = newBalance;
+	}
+
+	void print(){
+		writeToFile(cout);
+	}
+
+	void writeToFile(ostream& stream) {
+		stream<< firstName<< ", "<< lastName<< ", "<< balance<< endl;
+	}
+
+	void readName(ifstream& stream, char* name) {
+		char ch;
+		stream.get(ch);
+		int i;
+		for(i = 0; ch!=','; stream.get(ch))
+			name[i++] = ch;
+		name[i] = '\0';
+	}
+
+	void readFromFile(ifstream& stream) {
+		readName(stream, firstName);
+		readName(stream, lastName);
+		stream>> balance;
+		stream.ignore(5, '\n');
+	}
+};
+
+void strcpy(char* dest, const char* source) {
+	int i=0;
+	do
+		dest[i] = source[i];
+	while(source[i++] != '\0');
+	dest[i] = '\0';
+}
+
+int strlen(const char* str) {
+	int len = 0;
+	while(str[len++] != '\0');
+	return len;
+}
+
+// to be sure of no errors in format
+void generateFile() {
+	Client clients[5];
+	clients[0] = Client("Ivan", "Ivanov", 5.0);
+	clients[1] = Client("Pesho", "Toshev", 15.0);
+	clients[2] = Client("Asdf", "Petrov", 12.0);
+	clients[3] = Client("Thomas", "Anderson", 0.0);
+	clients[4] = Client("Test1", "Test2", 5.0);
+	ofstream clientsFile;
+	clientsFile.open("clients.txt", ios::out | ios::trunc);
+	for(int i=0; i<5; i++)
+		clients[i].writeToFile(clientsFile);
+	clientsFile.close();
+}
+
+int main () {
+	generateFile();
+	return 0;
+}
