784da84be55c0480b67dbf000185085042ecb9c7
dimiturtrz <dimitar.trz@gmail.com>
2017-03-12 22:54:06 +0200

    collinear points

--- hw3/geom.cpp
+++ hw3/geom.cpp
@@ -23,11 +23,21 @@ public:
 	Line(Point start, Point end): start(start), end(end) {}
 	Line(int x1, int y1, int x2, int y2): start(Point(x1, y1)), end(Point(x2, y2)){}
 
+	float slope() {
+		float slope = end.y - start.y;
+		slope /= (end.x - start.x);
+		return abs(slope);
+	}
+
 	bool operator||(Line& other) {
-		float slope1 = abs(((float)(end.y - start.y))/(end.x-start.x));
-		float slope2 = abs(((float)(other.end.y - other.start.y))/(other.end.x-other.start.x));
-		cout<< slope1<< " "<< slope2<< endl;
+		float slope1 = slope();
+		float slope2 = other.slope();
 		return (abs(slope1 - slope2) < 0.0001);
+	}
+
+	bool operator||(Point& point) {
+		Line compLine = Line(start, point);
+		return *this||compLine;
 	}
 };
 
@@ -37,6 +47,8 @@ int main () {
 
 	cout<< (line1||line2)<< endl;
 
+	Point p1(1,1);
+	cout<< (line1||p1)<< endl;
 	
 	return 0;
 }
