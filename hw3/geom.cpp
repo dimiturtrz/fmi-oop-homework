9ee3e0ca370053dc68de157b7d724710fe3bc5b3
dimiturtrz <dimitar.trz@gmail.com>
2017-03-17 23:32:33 +0200

    intersections

--- hw3/geom.cpp
+++ hw3/geom.cpp
@@ -14,6 +14,10 @@ struct Point {
 	Point(int x, int y): x(x), y(y){}
 };
 
+ostream& operator<<(ostream& stream, Point p) {
+	stream<< "("<< p.x<< ", "<< p.y<< ")";
+}
+
 class Line {
 private:
 	Point start;
@@ -23,21 +27,41 @@ public:
 	Line(Point start, Point end): start(start), end(end) {}
 	Line(int x1, int y1, int x2, int y2): start(Point(x1, y1)), end(Point(x2, y2)){}
 
-	float slope() {
+	float slope() const {
 		float slope = end.y - start.y;
 		slope /= (end.x - start.x);
-		return abs(slope);
+		return slope;
 	}
 
-	bool operator||(Line& other) {
+	float intercept() const {
+		return end.y - (end.x*slope());
+	}
+
+	bool operator||(Line& other) const {
 		float slope1 = slope();
 		float slope2 = other.slope();
 		return (abs(slope1 - slope2) < 0.0001);
 	}
 
-	bool operator||(Point& point) {
+	bool operator||(Point& point) const {
 		Line compLine = Line(start, point);
 		return *this||compLine;
+	}
+
+	Point operator+(Line& other) const {
+		if(*this||other)
+			return Point(0, 0);
+
+		const float slope1 = slope();
+		const float slope2 = other.slope();
+
+		const float intercept1 = intercept();
+		const float intercept2 = other.intercept();
+
+		float x = (intercept2 - intercept1)/(slope1 - slope2);
+		float y = intercept1 + x*slope1;
+		
+		return Point(x, y);
 	}
 };
 
@@ -49,6 +73,10 @@ int main () {
 
 	Point p1(1,1);
 	cout<< (line1||p1)<< endl;
+
+	Line line3 = Line(0, 0, 1, 1);
+	Line line4 = Line(0, 2, 2, 0);
+	cout<< (line3+line4)<< endl;
 	
 	return 0;
 }
