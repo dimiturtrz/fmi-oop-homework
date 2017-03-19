5051fc5998d6b8879dac15dc7d3532236ad4f944
dimiturtrz <dimitar.trz@gmail.com>
2017-03-19 12:32:29 +0200

    gcd and rat numbers addition

--- hw4/rat.cpp
+++ hw4/rat.cpp
@@ -1,14 +1,47 @@
 #include<iostream>
 using namespace std;
 
+/*template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}*/
+
 class Rat{
 private:
 	int num;
 	int denom;
+
+	int gcd(int a, int b) {
+		if(b > a)
+			swap(a, b);
+		while (b != 0) {
+			a %= b;
+			swap(a, b);
+		}
+
+		return a;
+	}
 public:
+
 	Rat(int num = 0, int denom = 0): num(num), denom(denom) {}
 
-	// TODO addition
+	int getNum() {
+		return num;
+	}
+
+	int getDenom() {
+		return denom;
+	}
+
+	Rat operator+(Rat& other) {
+		const int newDenom = (denom*other.denom)/gcd(denom, other.denom);
+		const int num1 = num*(newDenom/denom);
+		const int num2 = other.num*(newDenom/other.denom);
+
+		return Rat(num1 + num2, newDenom);
+	}
 
 	Rat operator*(Rat& other) {
 		Rat result(num, denom);
@@ -16,12 +49,14 @@ public:
 		result.denom *= other.denom;
 		return result;
 	}
-
-	
-
 };
 
-int main () {
+ostream& operator<<(ostream& stream, Rat rat) {
+	stream<< "("<< rat.getNum()<< "/"<< rat.getDenom()<< ")";
+}
 
+int main () {
+	Rat rat1(1,12), rat2(3,8);
+	cout<< (rat1+rat2)<< endl;
 	return 0;
 }
